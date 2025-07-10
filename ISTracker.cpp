b1ce1d3d5314acfeecebb004d766d4f3bc40259f
Christian Ruihi-Lewis <hamaka1206@gmail.com>
2025-07-10 20:28:27 +1200

    Update ISTracker.cpp
    Added adding entry and fixed interface problems
    Added viewing all

--- ISTracker.cpp
+++ ISTracker.cpp
@@ -10,12 +10,25 @@ using namespace std;
 
 vector<Content> contents;
 
+ostream& operator<<(ostream& os, const Content& c){
+			os << c.name << "\t" <<
+								c.organization << "\t" <<
+								c.website << "\t" <<
+								c.internship << "\t" <<
+								c.scholarship << "\t" <<
+								c.openDate << "\t" <<
+								c.closeDate << "\t" <<
+								c.startDate << "\t" <<
+								c.result;
+			return os;
+}
+
 
 bool saveList(){
 	ofstream ofile("InternshipsAndScholarships.txt");
 	if(!ofile.is_open()) return false;
 	for(auto & c : contents){
-		ofile<<c.toString()<<endl;
+		ofile<<c<<endl;
 	}
 	return true;
 }
@@ -33,23 +46,19 @@ bool loadList(){
 		if(!getline(ss, org, '\t'))return false;
 		string web;
 		if(!getline(ss, web, '\t'))return false;
-		bool in;
 		string intern;
 		if(!getline(ss, intern, '\t'))return false;
-		if(intern == "Yes") in = true;
-		else in = false;
-		bool s;
 		string schol;
 		if(!getline(ss, schol, '\t'))return false;
-		if(schol == "Yes") s = true;
-		else s = false;
 		string od;
 		if(!getline(ss, od, '\t'))return false;
 		string cd;
 		if(!getline(ss, cd, '\t'))return false;
 		string sd;
 		if(!getline(ss, sd, '\t'))return false;
-		Content c= Content(name, org, web, in, s, od, cd, sd);
+		string res;
+		if(!getline(ss, res, '\t'))return false;
+		Content c= Content(name, org, web, intern, schol, od, cd, sd, res);
 		contents.push_back(c);
 	}
 	return true;
@@ -59,7 +68,25 @@ bool interfaceExit(){
 	return saveList();
 }
 
+string CollectString(string msg){
+	cout<<msg<<endl;
+	string reply;
+	getline(cin, reply);
+	return reply;
+}
+
 void addEntry(){
+	string name = CollectString("Entry Name: ");
+	string org = CollectString("Entry Organization: ");
+	string web = CollectString("Entry Website: ");
+	string intern = CollectString("Is Entry an Internship? ");
+	string schol = CollectString("Is Entry a Scholarship? ");
+	string od = CollectString("Entry Opening Date: ");
+	string cd = CollectString("Entry Closing Date: ");
+	string sd = CollectString("Entry Starting Date: ");
+	string res = CollectString("Entry Current Status: ");
+	contents.push_back(Content(name, org, web, intern, schol, od, cd, sd, res));
+	
 	
 }
 
@@ -68,45 +95,45 @@ void viewNext(){
 }
 
 void viewAll(){
-	
+	for(auto it = contents.begin(); it != contents.end(); ++it){
+		cout<<*it<<endl;
+	}
 }
 
 bool interface(){
-	cout<<"What would you like to do?"<<
-		"- Add Entry"<<
-		"- View Next Due"<<
-		"- View All"<<
-		"- Exit"<<endl;
+	while (true) {
+		cout << "What would you like to do?" << endl
+		     << "- Add Entry" << endl
+		     << "- View Next Due" << endl
+		     << "- View All" << endl
+		     << "- Exit" << endl;
+
+		string command;
 		
-	string command;
-	cin>>command;
-	bool invalid = true;
-	while(invalid){
-		if(command == "Add Entry"){
-			 addEntry();
-			 invalid = false;
-		 }
-		else if(command == "View Next Due"){
-			 viewNext();
-			 invalid = false;
-		 }
-		else if(command == "View all"){
-			 viewAll();
-			 invalid = false;
-		 }
-		else if(command == "Exit"){
+		getline(cin, command);
+
+		if (command == "Add Entry") {
+			addEntry();
+		}
+		else if (command == "View Next Due") {
+			viewNext();
+		}
+		else if (command == "View All") {
+			viewAll();
+		}
+		else if (command == "Exit") {
 			return interfaceExit();
 		}
-		 else{
-			 cout<<"Please reply with: Add Entry, View Next Due, View All, or Exit"<<endl;
-		 }		
+		else {
+			cout <<"Command was: " << command << "Please reply with: Add Entry, View Next Due, View All, or Exit" << endl;
+		}
 	}
-	return interface();
 }
 
 
 int main(void){
-	if(!loadList()) exit(1);
-	if(!interface()) exit(2);
+	loadList();
+	interface();
 	
 }
+
